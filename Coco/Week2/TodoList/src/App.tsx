b3b943e17095bda78d0a1acedd7826ab2d39e970
kimyw1018 <poppppp00@gmail.com>
2025-09-23 01:24:47 +0900

    [week2/todoList] react+ts - useState

---
+++ Coco/Week2/TodoList/src/App.tsx
@@ -0,0 +1,15 @@
+import './APP.css';
+import Todo from'./components/Todo'
+import React from 'react';
+
+const App: React.FC = () =>{
+
+
+  return (
+    <>
+      <Todo />
+    </>
+  )
+};
+
+export default App
