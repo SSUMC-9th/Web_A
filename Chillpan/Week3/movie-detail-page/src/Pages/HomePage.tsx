82e6c9d1a726d05ddcc9d9acb6bbe700381fc2a1
taegeon2 <154239852+taegeon2@users.noreply.github.com>
2025-09-29 22:23:44 +0900

    [week3/mission] movie-list-detail

---
+++ Chillpan/Week3/movie-detail-page/src/Pages/HomePage.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-gray-300">
+          위의 탭을 클릭하여 영화를 탐색해보세요!
+        </h1>
+      </div>
+    </div>
+  );
+}
