0dd77916b3f72839a63ae6851de1ee94ed586306
YeoEunnn <ayeoreunmy3456@naver.com>
2025-12-16 16:44:43 +0900

    vercel 코드 수정

--- Rina/week10/movieSite/src/App.tsx
+++ Rina/week10/movieSite/src/App.tsx
@@ -1,13 +1,23 @@
 import HomePage from "./pages/HomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MovieDetailPage from "./pages/MovieDetailPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/movies/:movieId",
+    element: <MovieDetailPage />,
+  }
+])
 
 function App() {
 
   return (
-    <div
-      className="flex flex-col items-center"
-    >
-      <HomePage />
+    <div className="flex flex-col items-center" >
+      <RouterProvider router={router}/>
     </div>
   )
 }
